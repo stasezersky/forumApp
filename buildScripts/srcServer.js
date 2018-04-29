e2df81a10ed2d81ce0d83f79bed017952f5cf875
Stas Ezersky <stasezer@gmail.com>
2018-04-29 17:34:32 +0300

    addd 404 handling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -8,6 +8,9 @@ const app = express();
 app.get('/', function (req,res) {
     res.sendFile(path.join(__dirname,'../src/index.html'));
 });
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+})
 
 app.listen(port, (err) => {
     if(err){
